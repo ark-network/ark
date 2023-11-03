133807272dd28bc933bfd4090538be7156db9e47
Louis <louis@vulpem.com>
2023-11-03 15:41:17 +0100

    bip68_test.go: use require assertions

--- pkg/ark-sdk/bip68_test.go
+++ pkg/ark-sdk/bip68_test.go
@@ -6,50 +6,33 @@ import (
 	"testing"
 
 	sdk "github.com/ark-network/ark-sdk"
+	"github.com/stretchr/testify/require"
 )
 
 func TestBIP68(t *testing.T) {
-	// Load the fixture data from bip68.json
 	data, err := ioutil.ReadFile("fixtures/bip68.json")
-	if err != nil {
-		t.Fatalf("failed to read fixture data: %v", err)
-	}
+	require.NoError(t, err)
 
-	// Parse the fixture data into a slice of test cases
 	var testCases []struct {
 		Input    int    `json:"seconds"`
 		Expected int64  `json:"sequence"`
 		Desc     string `json:"description"`
 	}
-	if err := json.Unmarshal(data, &testCases); err != nil {
-		t.Fatalf("failed to parse fixture data: %v", err)
-	}
+	err = json.Unmarshal(data, &testCases)
+	require.NoError(t, err)
+	require.NotEmpty(t, testCases)
 
-	if len(testCases) == 0 {
-		t.Fatalf("no test cases found")
-	}
-
-	// Run each test case
 	for _, tc := range testCases {
 		t.Run(tc.Desc, func(t *testing.T) {
-			// Call the BIP68 function with the input from the test case
 			actual, err := sdk.BIP68(tc.Input)
-			if err != nil {
-				t.Fatalf("BIP68(%d) failed: %v", tc.Input, err)
-			}
+			require.NoError(t, err)
 
-			// bytes to int64
 			var asNumber int64
 			for i := len(actual) - 1; i >= 0; i-- {
 				asNumber = asNumber<<8 | int64(actual[i])
 			}
 
-			// Compare the actual result to the expected result from the test case
-			if asNumber != tc.Expected {
-				t.Errorf("BIP68(%d) = %v, expected %v", tc.Input, actual, tc.Expected)
-			} else {
-				t.Logf("BIP68(%d) = %v", tc.Input, actual)
-			}
+			require.Equal(t, tc.Expected, asNumber)
 		})
 	}
 }
