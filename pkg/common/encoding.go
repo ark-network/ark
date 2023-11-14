ea7fa4594380477a0d0a4904263dab14ba511ab7
altafan <18440657+altafan@users.noreply.github.com>
2023-11-14 17:07:34 +0100

    Fix Ark key encoding & Add new encoding for address, relay and url

--- pkg/common/encoding.go
+++ pkg/common/encoding.go
@@ -2,44 +2,167 @@ package common
 
 import (
 	"fmt"
+	"net/url"
+	"strings"
 
 	"github.com/btcsuite/btcd/btcutil/bech32"
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 )
 
 const (
-	PubKeyPrefix = "arkpub"
-	SecKeyPrefix = "arksec"
+	PubKeyPrefix  = "arkpub"
+	SecKeyPrefix  = "arksec"
+	AddressPrefix = "arkaddr"
+	RelayPrefix   = "arkrelay"
+	ProtoKey      = "ark"
+	RelayKey      = "relays"
+	RelaySep      = "-"
 )
 
 func EncodeSecKey(key *secp256k1.PrivateKey) (string, error) {
-	return bech32.Encode(SecKeyPrefix, key.Serialize())
+	if key == nil {
+		return "", fmt.Errorf("missing secret key")
+	}
+	grp, err := bech32.ConvertBits(key.Serialize(), 8, 5, true)
+	if err != nil {
+		return "", err
+	}
+	return bech32.Encode(SecKeyPrefix, grp)
 }
 
 func DecodeSecKey(key string) (*secp256k1.PrivateKey, error) {
-	prefix, buf, err := bech32.Decode(key)
+	hrp, buf, err := bech32.DecodeNoLimit(key)
+	if err != nil {
+		return nil, fmt.Errorf("invalid secret key: %s", err)
+	}
+	if hrp != SecKeyPrefix {
+		return nil, fmt.Errorf("invalid prefix")
+	}
+	grp, err := bech32.ConvertBits(buf, 5, 8, false)
 	if err != nil {
 		return nil, err
 	}
-	if prefix != SecKeyPrefix {
-		return nil, fmt.Errorf("invalid prefix")
-	}
-
-	return secp256k1.PrivKeyFromBytes(buf), nil
+	return secp256k1.PrivKeyFromBytes(grp), nil
 }
 
 func EncodePubKey(key *secp256k1.PublicKey) (string, error) {
-	return bech32.Encode(PubKeyPrefix, key.SerializeCompressed())
+	if key == nil {
+		return "", fmt.Errorf("missing public key")
+	}
+	grp, err := bech32.ConvertBits(key.SerializeCompressed(), 8, 5, true)
+	if err != nil {
+		return "", err
+	}
+	return bech32.Encode(PubKeyPrefix, grp)
 }
 
 func DecodePubKey(key string) (*secp256k1.PublicKey, error) {
-	prefix, buf, err := bech32.Decode(key)
+	hrp, buf, err := bech32.DecodeNoLimit(key)
 	if err != nil {
 		return nil, err
 	}
-	if prefix != PubKeyPrefix {
+	if hrp != PubKeyPrefix {
 		return nil, fmt.Errorf("invalid prefix")
 	}
+	grp, err := bech32.ConvertBits(buf, 5, 8, false)
+	if err != nil {
+		return nil, err
+	}
+	if len(grp) < 32 {
+		return nil, fmt.Errorf("invalid public key length")
+	}
+	return secp256k1.ParsePubKey(grp)
+}
 
-	return secp256k1.ParsePubKey(buf)
+func EncodeAddress(key *secp256k1.PublicKey) (string, error) {
+	if key == nil {
+		return "", fmt.Errorf("missing public key")
+	}
+	grp, err := bech32.ConvertBits(key.SerializeCompressed(), 8, 5, true)
+	if err != nil {
+		return "", err
+	}
+	return bech32.Encode(AddressPrefix, grp)
+}
+
+func DecodeAddress(addr string) (*secp256k1.PublicKey, error) {
+	hrp, buf, err := bech32.Decode(addr)
+	if err != nil {
+		return nil, err
+	}
+	if hrp != AddressPrefix {
+		return nil, fmt.Errorf("invalid prefix")
+	}
+	grp, err := bech32.ConvertBits(buf, 5, 8, false)
+	if err != nil {
+		return nil, err
+	}
+	return secp256k1.ParsePubKey(grp)
+}
+
+func EncodeRelayKey(key *secp256k1.PublicKey) (string, error) {
+	if key == nil {
+		return "", fmt.Errorf("missing relay key")
+	}
+	grp, err := bech32.ConvertBits(key.SerializeCompressed(), 8, 5, true)
+	if err != nil {
+		return "", err
+	}
+	return bech32.Encode(RelayPrefix, grp)
+}
+
+func DecodeRelayKey(key string) (*secp256k1.PublicKey, error) {
+	hrp, buf, err := bech32.DecodeNoLimit(key)
+	if err != nil {
+		return nil, err
+	}
+	if hrp != RelayPrefix {
+		return nil, fmt.Errorf("invalid prefix")
+	}
+	grp, err := bech32.ConvertBits(buf, 5, 8, false)
+	if err != nil {
+		return nil, err
+	}
+	if len(grp) < 32 {
+		return nil, fmt.Errorf("invalid public key length")
+	}
+	return secp256k1.ParsePubKey(grp)
+}
+
+func EncodeUrl(pubkey string, relays ...string) (string, error) {
+	if _, err := DecodePubKey(pubkey); err != nil {
+		return "", fmt.Errorf("invalid public key: %s", err)
+	}
+	for _, r := range relays {
+		if _, err := DecodeRelayKey(r); err != nil {
+			return "", fmt.Errorf("invalid relay public key: %s", err)
+		}
+	}
+	u := url.URL{Scheme: ProtoKey, Host: pubkey}
+	q := u.Query()
+	if len(relays) > 0 {
+		q.Add(RelayKey, strings.Join(relays, RelaySep))
+	}
+	u.RawQuery = q.Encode()
+	return u.String(), nil
+}
+
+func DecodeUrl(arkurl string) (string, []string, error) {
+	u, err := url.Parse(arkurl)
+	if err != nil {
+		return "", nil, err
+	}
+	if u.Scheme != ProtoKey {
+		return "", nil, fmt.Errorf("invalid proto")
+	}
+	if _, err := DecodePubKey(u.Host); err != nil {
+		return "", nil, fmt.Errorf("invalid public key: %s", err)
+	}
+	relays := strings.Split(u.Query().Get(RelayKey), RelaySep)
+	for _, r := range relays {
+		if _, err := DecodeRelayKey(r); err != nil {
+			return "", nil, fmt.Errorf("invalid relay public key: %s", err)
+		}
+	}
+	return u.Host, relays, nil
 }
---
+++ pkg/common/encoding_test.go
@@ -0,0 +1,225 @@
+package common_test
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"log"
+	"os"
+	"testing"
+
+	common "github.com/ark-network/ark/common"
+	"github.com/stretchr/testify/require"
+)
+
+var f []byte
+
+func init() {
+	var err error
+	f, err = os.ReadFile("fixtures/encoding.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestSecretKeyEncoding(t *testing.T) {
+	fixtures := struct {
+		SecretKey struct {
+			Valid []struct {
+				Key      string `json:"key"`
+				Expected string `json:"expected"`
+			} `json:"valid"`
+			Invalid []struct {
+				Key           string `json:"key"`
+				ExpectedError string `json:"expectedError"`
+			} `json:"invalid"`
+		} `json:"secretKey"`
+	}{}
+	err := json.Unmarshal(f, &fixtures)
+	require.NoError(t, err)
+
+	t.Run("valid", func(t *testing.T) {
+		for _, f := range fixtures.SecretKey.Valid {
+			key, err := common.DecodeSecKey(f.Key)
+			require.NoError(t, err)
+			require.NotEmpty(t, key)
+
+			keyHex := hex.EncodeToString(key.Serialize())
+			require.Equal(t, keyHex, f.Expected)
+
+			keyStr, err := common.EncodeSecKey(key)
+			require.NoError(t, err)
+			require.Equal(t, keyStr, f.Key)
+		}
+	})
+
+	t.Run("invalid", func(t *testing.T) {
+		for _, f := range fixtures.SecretKey.Invalid {
+			key, err := common.DecodeSecKey(f.Key)
+			require.EqualError(t, err, f.ExpectedError)
+			require.Empty(t, key)
+		}
+	})
+}
+
+func TestPublicKeyEncoding(t *testing.T) {
+	fixtures := struct {
+		PublicKey struct {
+			Valid []struct {
+				Key      string `json:"key"`
+				Expected string `json:"expected"`
+			} `json:"valid"`
+			Invalid []struct {
+				Key           string `json:"key"`
+				ExpectedError string `json:"expectedError"`
+			} `json:"invalid"`
+		} `json:"publicKey"`
+	}{}
+	err := json.Unmarshal(f, &fixtures)
+	require.NoError(t, err)
+
+	t.Run("valid", func(t *testing.T) {
+		for _, f := range fixtures.PublicKey.Valid {
+			key, err := common.DecodePubKey(f.Key)
+			require.NoError(t, err)
+			require.NotNil(t, key)
+
+			keyHex := hex.EncodeToString(key.SerializeCompressed())
+			require.Equal(t, keyHex, f.Expected)
+
+			keyStr, err := common.EncodePubKey(key)
+			require.NoError(t, err)
+			require.Equal(t, keyStr, f.Key)
+		}
+	})
+
+	t.Run("invalid", func(t *testing.T) {
+		for _, f := range fixtures.PublicKey.Invalid {
+			key, err := common.DecodePubKey(f.Key)
+			require.EqualError(t, err, f.ExpectedError)
+			require.Nil(t, key)
+		}
+	})
+}
+
+func TestAddressEncoding(t *testing.T) {
+	fixtures := struct {
+		Address struct {
+			Valid []struct {
+				Addr     string `json:"addr"`
+				Expected string `json:"expected"`
+			} `json:"valid"`
+			Invalid []struct {
+				Addr          string `json:"addr"`
+				ExpectedError string `json:"expectedError"`
+			} `json:"invalid"`
+		} `json:"address"`
+	}{}
+	err := json.Unmarshal(f, &fixtures)
+	require.NoError(t, err)
+
+	t.Run("valid", func(t *testing.T) {
+		for _, f := range fixtures.Address.Valid {
+			key, err := common.DecodeAddress(f.Addr)
+			require.NoError(t, err)
+			require.NotNil(t, key)
+
+			keyHex := hex.EncodeToString(key.SerializeCompressed())
+			require.Equal(t, keyHex, f.Expected)
+
+			addr, err := common.EncodeAddress(key)
+			require.NoError(t, err)
+			require.Equal(t, addr, f.Addr)
+		}
+	})
+
+	t.Run("invalid", func(t *testing.T) {
+		for _, f := range fixtures.Address.Invalid {
+			key, err := common.DecodeAddress(f.Addr)
+			require.EqualError(t, err, f.ExpectedError)
+			require.Nil(t, key)
+		}
+	})
+}
+
+func TestRelayKeyEncoding(t *testing.T) {
+	fixtures := struct {
+		RelayKey struct {
+			Valid []struct {
+				Key      string `json:"key"`
+				Expected string `json:"expected"`
+			} `json:"valid"`
+			Invalid []struct {
+				Key           string `json:"key"`
+				ExpectedError string `json:"expectedError"`
+			} `json:"invalid"`
+		} `json:"relayKey"`
+	}{}
+	err := json.Unmarshal(f, &fixtures)
+	require.NoError(t, err)
+
+	t.Run("valid", func(t *testing.T) {
+		for _, f := range fixtures.RelayKey.Valid {
+			key, err := common.DecodeRelayKey(f.Key)
+			require.NoError(t, err)
+			require.NotNil(t, key)
+
+			keyHex := hex.EncodeToString(key.SerializeCompressed())
+			require.Equal(t, keyHex, f.Expected)
+
+			keyStr, err := common.EncodeRelayKey(key)
+			require.NoError(t, err)
+			require.Equal(t, keyStr, f.Key)
+		}
+	})
+
+	t.Run("invalid", func(t *testing.T) {
+		for _, f := range fixtures.RelayKey.Invalid {
+			key, err := common.DecodeRelayKey(f.Key)
+			require.EqualError(t, err, f.ExpectedError)
+			require.Nil(t, key)
+		}
+	})
+}
+
+func TestUrlEncoding(t *testing.T) {
+	fixtures := struct {
+		Url struct {
+			Valid []struct {
+				Url            string   `json:"url"`
+				ExpectedPubkey string   `json:"expectedPubkey"`
+				ExpectedRelays []string `json:"expectedRelays"`
+			} `json:"valid"`
+			Invalid []struct {
+				Url           string `json:"url"`
+				ExpectedError string `json:"expectedError"`
+			} `json:"invalid"`
+		} `json:"url"`
+	}{}
+	err := json.Unmarshal(f, &fixtures)
+	require.NoError(t, err)
+
+	t.Run("valid", func(t *testing.T) {
+		for _, f := range fixtures.Url.Valid {
+			pubkey, relays, err := common.DecodeUrl(f.Url)
+			require.NoError(t, err)
+			require.NotEmpty(t, pubkey)
+			require.NotNil(t, relays)
+
+			require.Equal(t, pubkey, f.ExpectedPubkey)
+			require.Exactly(t, relays, f.ExpectedRelays)
+
+			url, err := common.EncodeUrl(pubkey, relays...)
+			require.NoError(t, err)
+			require.Equal(t, url, f.Url)
+		}
+	})
+
+	t.Run("invalid", func(t *testing.T) {
+		for _, f := range fixtures.Url.Invalid {
+			pubkey, relays, err := common.DecodeUrl(f.Url)
+			require.Contains(t, err.Error(), f.ExpectedError)
+			require.Empty(t, pubkey)
+			require.Nil(t, relays)
+		}
+	})
+}
