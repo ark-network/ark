d86c382f2675bc7d373a5fc2eb6427a9b60b7557
altafan <18440657+altafan@users.noreply.github.com>
2023-11-22 16:29:48 +0100

    Add methods to spend, claim and sign vtxos

--- internal/core/application/service.go
+++ internal/core/application/service.go
@@ -9,13 +9,17 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+const paymentsThreshold = 128
+
 type service struct {
 	roundInterval int64
 
-	wallet      ports.WalletService
-	scheduler   ports.SchedulerService
-	repoManager ports.RepoManager
-	builder     ports.TxBuilder
+	wallet          ports.WalletService
+	scheduler       ports.SchedulerService
+	repoManager     ports.RepoManager
+	builder         ports.TxBuilder
+	paymentRequests *paymentsMap
+	forfeitTxs      *forfeitTxsMap
 }
 
 func NewService(
@@ -23,11 +27,71 @@ func NewService(
 	walletSvc ports.WalletService, schedulerSvc ports.SchedulerService,
 	repoManager ports.RepoManager, builder ports.TxBuilder,
 ) *service {
-	return &service{interval, walletSvc, schedulerSvc, repoManager, builder}
+	paymentRequests := newPaymentsMap(nil)
+	forfeitTxs := newForfeitTxsMap()
+	return &service{
+		interval, walletSvc, schedulerSvc, repoManager, builder,
+		paymentRequests, forfeitTxs,
+	}
 }
 
 func (s *service) Start() {
 	s.start()
+}
+
+func (s *service) SpendVtxos(ctx context.Context, inputs []domain.VtxoKey) (string, error) {
+	vtxos, err := s.repoManager.Vtxos().GetVtxos(ctx, inputs)
+	if err != nil {
+		return "", err
+	}
+	for _, v := range vtxos {
+		if v.Spent {
+			return "", fmt.Errorf("input %s:%d already spent", v.Txid, v.VOut)
+		}
+	}
+
+	payment := domain.NewPayment(inputs)
+	if err := s.paymentRequests.push(payment); err != nil {
+		return "", err
+	}
+	return payment.Id, nil
+}
+
+func (s *service) ClaimVtxos(ctx context.Context, creds string, receivers []domain.Receiver) error {
+	// Check credentials
+	payment, ok := s.paymentRequests.view(creds)
+	if !ok {
+		return fmt.Errorf("invalid credentials")
+	}
+
+	// Check that input and output and output amounts match.
+	vtxos, err := s.repoManager.Vtxos().GetVtxos(ctx, payment.Inputs)
+	if err != nil {
+		return err
+	}
+	inAmount := uint64(0)
+	for _, v := range vtxos {
+		inAmount += v.Amount
+	}
+	outAmount := uint64(0)
+	for _, v := range receivers {
+		outAmount += v.Amount
+	}
+	if inAmount != outAmount {
+		return fmt.Errorf("input and output amounts mismatch")
+	}
+
+	payment.Receivers = receivers
+	return s.paymentRequests.update(*payment)
+}
+
+func (s *service) SignVtxos(ctx context.Context, forfeitTxs map[string]string) error {
+	for txid, tx := range forfeitTxs {
+		if err := s.forfeitTxs.sign(txid, tx); err != nil {
+			return fmt.Errorf("invalid forfeit tx %s: %s", txid, err)
+		}
+	}
+	return nil
 }
 
 func (s *service) start() {
@@ -60,38 +124,44 @@ func (s *service) startFinalization() {
 		return
 	}
 
-	allPayments := make([]domain.Payment, 0, len(round.Payments))
-	for _, p := range round.Payments {
-		allPayments = append(allPayments, p)
+	// TODO: understand how many payments must be popped from the queue and actually registered for the round
+	num := s.paymentRequests.len()
+	if num > paymentsThreshold {
+		num = paymentsThreshold
 	}
+	payments := s.paymentRequests.pop(num)
+	changes, _ := round.RegisterPayments(payments)
 
-	signedPoolTx, err := s.builder.BuildPoolTx(s.wallet, allPayments)
+	signedPoolTx, err := s.builder.BuildPoolTx(s.wallet, payments)
 	if err != nil {
 		round.Fail(fmt.Errorf("failed to create pool tx: %s", err))
 		log.WithError(err).Warn("failed to create pool tx")
 		return
 	}
 
-	tree, err := s.builder.BuildCongestionTree(signedPoolTx, allPayments)
+	tree, err := s.builder.BuildCongestionTree(signedPoolTx, payments)
 	if err != nil {
 		round.Fail(fmt.Errorf("failed to create congestion tree: %s", err))
 		log.WithError(err).Warn("failed to create congestion tree")
 		return
 	}
 
-	connectors, forfeitTxs, err := s.builder.BuildForfeitTxs(signedPoolTx, allPayments)
+	connectors, forfeitTxs, err := s.builder.BuildForfeitTxs(signedPoolTx, payments)
 	if err != nil {
 		round.Fail(fmt.Errorf("failed to create connectors and forfeit txs: %s", err))
 		log.WithError(err).Warn("failed to create connectors and forfeit txs")
 		return
 	}
 
-	changes, _ := round.StartFinalization(connectors, forfeitTxs, tree, signedPoolTx)
+	events, _ := round.StartFinalization(connectors, tree, signedPoolTx)
+	changes = append(changes, events...)
 
 	if err := s.repoManager.Events().Save(ctx, changes...); err != nil {
 		log.WithError(err).Warn("failed to store new round events")
 		return
 	}
+
+	s.forfeitTxs.push(forfeitTxs)
 
 	log.Debugf("started finalization stage for round: %s", round.Id)
 }
@@ -107,6 +177,14 @@ func (s *service) finalizeRound() {
 		return
 	}
 
+	forfeitTxs, leftUnsigned := s.forfeitTxs.pop()
+	if len(leftUnsigned) > 0 {
+		err := fmt.Errorf("%d forfeit txs left so sign", len(leftUnsigned))
+		round.Fail(fmt.Errorf("failed to finalize round: %s", err))
+		log.WithError(err).Warn("failed to finalize round")
+		return
+	}
+
 	txid, err := s.wallet.Transaction().BroadcastTransaction(ctx, round.TxHex)
 	if err != nil {
 		round.Fail(fmt.Errorf("failed to broadcast pool tx: %s", err))
@@ -114,7 +192,7 @@ func (s *service) finalizeRound() {
 		return
 	}
 
-	changes, _ := round.EndFinalization(txid)
+	changes, _ := round.EndFinalization(forfeitTxs, txid)
 	if err := s.repoManager.Events().Save(ctx, changes...); err != nil {
 		log.WithError(err).Warn("failed to store new round events")
 		return
