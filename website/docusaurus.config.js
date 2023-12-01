758fef9bc5a35b47ab639d1596208045ea7591ab
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2023-12-01 12:35:54 +0100

    Update website/docusaurus.config.js
    Co-authored-by: João Bordalo <bordalix@users.noreply.github.com>
    Signed-off-by: Marco Argentieri <3596602+tiero@users.noreply.github.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -141,7 +141,7 @@ const config = {
       colorMode: {
         defaultMode: 'light',
         disableSwitch: false,
-        respectPrefersColorScheme: false,
+        respectPrefersColorScheme: true,
       },
       prism: {
         theme: lightCodeTheme,
