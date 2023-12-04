faff80f211fa7dcc30cdaeba97234a912e91bfd0
João Bordalo <bordalix@users.noreply.github.com>
2023-12-04 16:45:30 +0000

    adds mermaid support

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -151,6 +151,10 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
 }
 
 module.exports = config
