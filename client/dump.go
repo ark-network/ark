b1e4317cebec5d72929b7f093f96b60315850252
João Bordalo <bordalix@users.noreply.github.com>
2024-02-12 12:40:00 +0000

    adds dumpPrivKey command

---
+++ client/dump.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/hex"
+
+	"github.com/urfave/cli/v2"
+)
+
+var dumpCommand = cli.Command{
+	Name:   "dumpPrivKey",
+	Usage:  "Dump private key of the Ark wallet",
+	Action: dumpAction,
+}
+
+func dumpAction(ctx *cli.Context) error {
+  privateKey, err := privateKeyFromPassword()
+	if err != nil {
+		return err
+	}
+
+	return printJSON(map[string]interface{}{
+		"privateKey": hex.EncodeToString(privateKey.Serialize()),
+	})
+}
