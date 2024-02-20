0c2229d6fc1aea2b989ce6647f0060975033d19d
Louis Singer <louis@vulpem.com>
2024-02-20 13:14:32 +0100

    remove pubkey/relaykey/url encoding in common pkg

--- common/encoding.go
+++ common/encoding.go
@@ -2,60 +2,10 @@ package common
 
 import (
 	"fmt"
-	"net/url"
-	"strings"
 
 	"github.com/btcsuite/btcd/btcutil/bech32"
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 )
-
-const (
-	ProtoKey = "ark"
-	RelayKey = "relays"
-	RelaySep = "-"
-)
-
-func EncodePubKey(hrp string, key *secp256k1.PublicKey) (pubkey string, err error) {
-	if key == nil {
-		err = fmt.Errorf("missing public key")
-		return
-	}
-	if hrp != MainNet.PubKey && hrp != TestNet.PubKey {
-		err = fmt.Errorf("invalid prefix")
-		return
-	}
-	grp, err := bech32.ConvertBits(key.SerializeCompressed(), 8, 5, true)
-	if err != nil {
-		return
-	}
-	pubkey, err = bech32.EncodeM(hrp, grp)
-	return
-}
-
-func DecodePubKey(key string) (hrp string, pubkey *secp256k1.PublicKey, err error) {
-	prefix, buf, err := bech32.DecodeNoLimit(key)
-	if err != nil {
-		return
-	}
-	if prefix != MainNet.PubKey && prefix != TestNet.PubKey {
-		err = fmt.Errorf("invalid prefix")
-		return
-	}
-	grp, err := bech32.ConvertBits(buf, 5, 8, false)
-	if err != nil {
-		return
-	}
-	if len(grp) < 32 {
-		err = fmt.Errorf("invalid public key length")
-		return
-	}
-	pubkey, err = secp256k1.ParsePubKey(grp)
-	if err != nil {
-		return
-	}
-	hrp = prefix
-	return
-}
 
 func EncodeAddress(hrp string, userKey, aspKey *secp256k1.PublicKey) (addr string, err error) {
 	if userKey == nil {
@@ -105,98 +55,5 @@ func DecodeAddress(addr string) (hrp string, userKey *secp256k1.PublicKey, aspKe
 	hrp = prefix
 	userKey = uKey
 	aspKey = aKey
-	return
-}
-
-func EncodeRelayKey(hrp string, key *secp256k1.PublicKey) (pubkey string, err error) {
-	if key == nil {
-		err = fmt.Errorf("missing relay key")
-		return
-	}
-	if hrp != MainNet.RelayKey && hrp != TestNet.RelayKey {
-		err = fmt.Errorf("invalid prefix")
-		return
-	}
-	grp, err := bech32.ConvertBits(key.SerializeCompressed(), 8, 5, true)
-	if err != nil {
-		return
-	}
-	pubkey, err = bech32.EncodeM(hrp, grp)
-	return
-}
-
-func DecodeRelayKey(key string) (hrp string, pubkey *secp256k1.PublicKey, err error) {
-	prefix, buf, err := bech32.DecodeNoLimit(key)
-	if err != nil {
-		return
-	}
-	if prefix != MainNet.RelayKey && prefix != TestNet.RelayKey {
-		err = fmt.Errorf("invalid prefix")
-		return
-	}
-	grp, err := bech32.ConvertBits(buf, 5, 8, false)
-	if err != nil {
-		return
-	}
-	if len(grp) < 32 {
-		err = fmt.Errorf("invalid public key length")
-		return
-	}
-	pubkey, err = secp256k1.ParsePubKey(grp)
-	if err != nil {
-		return
-	}
-	hrp = prefix
-	return
-}
-
-func EncodeUrl(host string, relays ...string) (arkurl string, err error) {
-	_, _, err = DecodePubKey(host)
-	if err != nil {
-		err = fmt.Errorf("invalid public key: %s", err)
-		return
-	}
-	for _, r := range relays {
-		_, _, err = DecodeRelayKey(r)
-		if err != nil {
-			err = fmt.Errorf("invalid relay public key: %s", err)
-			return
-		}
-	}
-	u := url.URL{Scheme: ProtoKey, Host: host}
-	q := u.Query()
-	if len(relays) > 0 {
-		q.Add(RelayKey, strings.Join(relays, RelaySep))
-	}
-	u.RawQuery = q.Encode()
-	arkurl = u.String()
-	return
-}
-
-func DecodeUrl(arkurl string) (host string, relays []string, err error) {
-	u, err := url.Parse(arkurl)
-	if err != nil {
-		return
-	}
-	if u.Scheme != ProtoKey {
-		err = fmt.Errorf("invalid proto")
-		return
-	}
-	_, _, err = DecodePubKey(u.Host)
-	if err != nil {
-		err = fmt.Errorf("invalid public key: %s", err)
-		return
-	}
-	list := strings.Split(u.Query().Get(RelayKey), RelaySep)
-	for _, r := range list {
-		_, _, err = DecodeRelayKey(r)
-		if err != nil {
-			err = fmt.Errorf("invalid relay public key: %s", err)
-			return
-		}
-	}
-	host = u.Host
-	relays = make([]string, len(list))
-	copy(relays, list)
 	return
 }
--- common/network.go
+++ common/network.go
@@ -1,23 +1,17 @@
 package common
 
 type Network struct {
-	Name     string
-	SecKey   string
-	PubKey   string
-	RelayKey string
-	Addr     string
+	Name   string
+	SecKey string
+	Addr   string
 }
 
 var MainNet = Network{
-	Name:     "mainnet",
-	PubKey:   "apub",
-	RelayKey: "arelay",
-	Addr:     "ark",
+	Name: "mainnet",
+	Addr: "ark",
 }
 
 var TestNet = Network{
-	Name:     "testnet",
-	PubKey:   "tapub",
-	RelayKey: "tarelay",
-	Addr:     "tark",
+	Name: "testnet",
+	Addr: "tark",
 }
