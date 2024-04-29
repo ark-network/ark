dc64947d282d32b25e77d77135e41844325fec5d
Louis Singer <41042567+louisinger@users.noreply.github.com>
2024-04-29 18:58:11 +0200

    returns forfeitsTxs in Ping RPC while the finalization started (#161)

--- server/internal/interface/grpc/handlers/arkservice.go
+++ server/internal/interface/grpc/handlers/arkservice.go
@@ -107,11 +107,14 @@ func (h *handler) Ping(ctx context.Context, req *arkv1.PingRequest) (*arkv1.Ping
 		return nil, status.Error(codes.InvalidArgument, "missing payment id")
 	}
 
-	if err := h.svc.UpdatePaymentStatus(ctx, req.GetPaymentId()); err != nil {
+	forfeits, err := h.svc.UpdatePaymentStatus(ctx, req.GetPaymentId())
+	if err != nil {
 		return nil, err
 	}
 
-	return &arkv1.PingResponse{}, nil
+	return &arkv1.PingResponse{
+		ForfeitTxs: forfeits,
+	}, nil
 }
 
 func (h *handler) RegisterPayment(ctx context.Context, req *arkv1.RegisterPaymentRequest) (*arkv1.RegisterPaymentResponse, error) {
