34aff2c4c3bce6026bb8c09f91bec642b1ab9995
Louis Singer <41042567+louisinger@users.noreply.github.com>
2024-05-06 18:09:11 +0200

    ListVtxos rpc: return swept vtxos (#168)

--- server/internal/core/domain/round_repo.go
+++ server/internal/core/domain/round_repo.go
@@ -25,5 +25,6 @@ type VtxoRepository interface {
 	GetVtxosForRound(ctx context.Context, txid string) ([]Vtxo, error)
 	SweepVtxos(ctx context.Context, vtxos []VtxoKey) error
 	GetAllVtxos(ctx context.Context, pubkey string) ([]Vtxo, []Vtxo, error)
+	GetAllSweepableVtxos(ctx context.Context) ([]Vtxo, error)
 	UpdateExpireAt(ctx context.Context, vtxos []VtxoKey, expireAt int64) error
 }
