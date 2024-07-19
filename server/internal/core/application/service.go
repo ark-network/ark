5b2e11b81129b820b3ddb8d08b2706bd14ec2ff7
altafan <18440657+altafan@users.noreply.github.com>
2024-07-19 10:28:25 +0200

    Add bitcoin networks

--- server/internal/core/application/service.go
+++ server/internal/core/application/service.go
@@ -436,8 +436,6 @@ func (s *service) finalizeRound() {
 		return
 	}
 
-	fmt.Printf("%+v\n", *round)
-
 	var changes []domain.RoundEvent
 	defer func() {
 		if err := s.saveEvents(ctx, round.Id, changes); err != nil {
