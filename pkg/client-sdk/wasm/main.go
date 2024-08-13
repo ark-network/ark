2932d0cb6b460f16835db5b72f42e42ff5cc4774
tiero <3596602+tiero@users.noreply.github.com>
2024-08-13 15:43:15 +0200

    refactor sdk wasm

--- pkg/client-sdk/wasm/main.go
+++ pkg/client-sdk/wasm/main.go
@@ -4,13 +4,38 @@
 package main
 
 import (
+	"context"
+	inmemorystore "github.com/ark-network/ark/pkg/client-sdk/store/inmemory"
 	"github.com/ark-network/ark/pkg/client-sdk/wasm/browser"
 )
 
 func main() {
 	c := make(chan struct{}, 0)
+	var (
+		ctx = context.Background()
+	)
+	store, err := browser.NewLocalStorageStore()
+	if err != nil {
+		browser.ConsoleError(err)
+		return
+	}
+	if store != nil {
+		if err := browser.NewCovenantlessClient(ctx, store); err != nil {
+			browser.ConsoleError(err)
+			return
+		}
+	} else {
+		storeSvc, err := inmemorystore.NewConfigStore()
+		if err != nil {
+			browser.ConsoleError(err)
+			return
+		}
+		if err := browser.NewCovenantlessClient(ctx, storeSvc); err != nil {
+			browser.ConsoleError(err)
+			return
+		}
+	}
 	println("ARK SDK WebAssembly module initialized")
-	browser.InitWrapper()
 	<-c
 }
 
