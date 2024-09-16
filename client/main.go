2a430320c8af29c24a362f47ce7e7a742a5331b5
sekulicd <sekula87@gmail.com>
2024-09-16 18:56:47 +0200

    merge with master

--- client/main.go
+++ client/main.go
@@ -221,10 +221,6 @@ func config(ctx *cli.Context) error {
 	if err != nil {
 		return err
 	}
-	if cfgData == nil {
-		fmt.Println("no configuration found, run 'init' command")
-		return nil
-	}
 
 	cfg := map[string]interface{}{
 		"asp_url":                      cfgData.AspUrl,
