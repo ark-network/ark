269cb6da6f12a6d706872598498cb1c87824be24
altafan <18440657+altafan@users.noreply.github.com>
2024-09-26 17:00:18 +0200

    Ignore logs below warning level

--- server/internal/infrastructure/wallet/btc-embedded/wallet.go
+++ server/internal/infrastructure/wallet/btc-embedded/wallet.go
@@ -1299,5 +1299,7 @@ func fromOutputScript(script []byte, netParams *chaincfg.Params) (btcutil.Addres
 }
 
 func logger(subsystem string) btclog.Logger {
-	return btclog.NewBackend(log.StandardLogger().Writer()).Logger(subsystem)
+	logger := btclog.NewBackend(log.StandardLogger().Writer()).Logger(subsystem)
+	logger.SetLevel(btclog.LevelWarn)
+	return logger
 }
