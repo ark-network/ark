698ba4bf0228412642e2b22407f5c6240acf9413
Dusan Sekulic <sekula87@gmail.com>
2024-11-14 15:09:10 +0100

    Fix nonce encoding/decoding & Fix pop from queue of payment requests (#375)
    * bug fix - paymentMap pop input traversal
    
    * bug fix - nonce encode/decode
    
    * pr review
    
    * pr review

--- server/cmd/arkd/main.go
+++ server/cmd/arkd/main.go
@@ -88,6 +88,8 @@ func mainAction(_ *cli.Context) error {
 		return err
 	}
 
+	log.Infof("Ark Server config: %+v", appConfig)
+
 	log.Info("starting service...")
 	if err := svc.Start(); err != nil {
 		return err
