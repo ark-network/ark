49ceb49112487ece59d321447226160f21d96f19
altafan <18440657+altafan@users.noreply.github.com>
2024-11-22 16:04:51 +0100

    Fix

--- server/internal/infrastructure/db/badger/vtxo_repo.go
+++ server/internal/infrastructure/db/badger/vtxo_repo.go
@@ -100,7 +100,7 @@ func (r *vtxoRepository) GetAllVtxos(
 ) ([]domain.Vtxo, []domain.Vtxo, error) {
 	query := badgerhold.Where("Redeemed").Eq(false)
 	if len(pubkey) > 0 {
-		query = query.And("Pubkey").Eq(pubkey)
+		query = query.And("PubKey").Eq(pubkey)
 	}
 	vtxos, err := r.findVtxos(ctx, query)
 	if err != nil {
