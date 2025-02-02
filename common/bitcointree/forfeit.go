f7fd2f5f1aa1f67682cb719823f7cb0041fa1162
altafan <18440657+altafan@users.noreply.github.com>
2025-02-02 02:08:19 +0100

    Concurrent BuildForfeitTxs

--- common/bitcointree/forfeit.go
+++ common/bitcointree/forfeit.go
@@ -1,6 +1,8 @@
 package bitcointree
 
 import (
+	"sync"
+
 	"github.com/btcsuite/btcd/btcutil/psbt"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
@@ -11,56 +13,79 @@ func BuildForfeitTxs(
 	vtxoAmount, connectorAmount, feeAmount uint64,
 	vtxoScript, serverScript []byte,
 	txLocktime uint32,
-) (forfeitTxs []*psbt.Packet, err error) {
+) ([]*psbt.Packet, error) {
 	version := int32(2)
 	connectors, prevouts := getConnectorInputs(connectorTx, int64(connectorAmount))
+	forfeitTxs := make([]*psbt.Packet, len(connectors))
+	chTxs := make(chan chTx, len(connectors))
 
-	for i, connectorInput := range connectors {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(connectors))
+	go func() {
+		wg.Wait()
+		close(chTxs)
+	}()
+
+	for i := range connectors {
 		connectorPrevout := prevouts[i]
+		connectorInput := connectors[i]
 
-		ins := []*wire.OutPoint{connectorInput, vtxoInput}
-		outs := []*wire.TxOut{{
-			Value:    int64(vtxoAmount) + int64(connectorAmount) - int64(feeAmount),
-			PkScript: serverScript,
-		}}
+		go func(i int, connectorInput *wire.OutPoint, connectorPrevout *wire.TxOut) {
+			defer wg.Done()
 
-		vtxoSequence := wire.MaxTxInSequenceNum
-		if txLocktime != 0 {
-			vtxoSequence = wire.MaxTxInSequenceNum - 1
+			ins := []*wire.OutPoint{connectorInput, vtxoInput}
+			outs := []*wire.TxOut{{
+				Value:    int64(vtxoAmount) + int64(connectorAmount) - int64(feeAmount),
+				PkScript: serverScript,
+			}}
+
+			vtxoSequence := wire.MaxTxInSequenceNum
+			if txLocktime != 0 {
+				vtxoSequence = wire.MaxTxInSequenceNum - 1
+			}
+			txSequence := []uint32{wire.MaxTxInSequenceNum, vtxoSequence}
+
+			partialTx, err := psbt.New(
+				ins, outs, version, txLocktime, txSequence,
+			)
+			if err != nil {
+				chTxs <- chTx{-1, nil, err}
+				return
+			}
+
+			updater, err := psbt.NewUpdater(partialTx)
+			if err != nil {
+				chTxs <- chTx{-1, nil, err}
+				return
+			}
+
+			if err := updater.AddInWitnessUtxo(connectorPrevout, 0); err != nil {
+				chTxs <- chTx{-1, nil, err}
+				return
+			}
+
+			if err := updater.AddInWitnessUtxo(&wire.TxOut{
+				Value:    int64(vtxoAmount),
+				PkScript: vtxoScript,
+			}, 1); err != nil {
+				chTxs <- chTx{-1, nil, err}
+				return
+			}
+
+			if err := updater.AddInSighashType(txscript.SigHashDefault, 1); err != nil {
+				chTxs <- chTx{-1, nil, err}
+				return
+			}
+
+			chTxs <- chTx{i, partialTx, nil}
+		}(i, connectorInput, connectorPrevout)
+	}
+
+	for c := range chTxs {
+		if c.err != nil {
+			return nil, c.err
 		}
-
-		partialTx, err := psbt.New(
-			ins,
-			outs,
-			version,
-			txLocktime,
-			[]uint32{wire.MaxTxInSequenceNum, vtxoSequence},
-		)
-		if err != nil {
-			return nil, err
-		}
-
-		updater, err := psbt.NewUpdater(partialTx)
-		if err != nil {
-			return nil, err
-		}
-
-		if err := updater.AddInWitnessUtxo(connectorPrevout, 0); err != nil {
-			return nil, err
-		}
-
-		if err := updater.AddInWitnessUtxo(&wire.TxOut{
-			Value:    int64(vtxoAmount),
-			PkScript: vtxoScript,
-		}, 1); err != nil {
-			return nil, err
-		}
-
-		if err := updater.AddInSighashType(txscript.SigHashDefault, 1); err != nil {
-			return nil, err
-		}
-
-		forfeitTxs = append(forfeitTxs, partialTx)
+		forfeitTxs[c.i] = c.tx
 	}
 
 	return forfeitTxs, nil
@@ -81,4 +106,10 @@ func getConnectorInputs(partialTx *psbt.Packet, connectorAmount int64) ([]*wire.
 	}
 
 	return inputs, witnessUtxos
+}
+
+type chTx struct {
+	i   int
+	tx  *psbt.Packet
+	err error
 }
