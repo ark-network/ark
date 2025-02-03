ea6b0680d4e3e9a2931bae71415978742b6da103
altafan <18440657+altafan@users.noreply.github.com>
2025-02-03 19:05:21 +0100

    Revert all changes

--- common/bitcointree/forfeit.go
+++ common/bitcointree/forfeit.go
@@ -1,10 +1,6 @@
 package bitcointree
 
 import (
-	"math"
-	"runtime"
-	"sync"
-
 	"github.com/btcsuite/btcd/btcutil/psbt"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
@@ -15,105 +11,59 @@ func BuildForfeitTxs(
 	vtxoAmount, connectorAmount, feeAmount uint64,
 	vtxoScript, serverScript []byte,
 	txLocktime uint32,
-) ([]*psbt.Packet, error) {
+) (forfeitTxs []*psbt.Packet, err error) {
 	version := int32(2)
 	connectors, prevouts := getConnectorInputs(connectorTx, int64(connectorAmount))
-	nbWorkers := int(math.Min(float64(runtime.NumCPU()), float64(len(connectors))))
-	forfeitTxs := make([]*psbt.Packet, len(connectors))
-	jobs := make(chan chJob, len(connectors))
-	chErr := make(chan error, 1)
-	chTxs := make(chan chTx, len(connectors))
 
-	wg := sync.WaitGroup{}
-	wg.Add(len(connectors))
+	for i, connectorInput := range connectors {
+		connectorPrevout := prevouts[i]
 
-	for i := 0; i < nbWorkers; i++ {
-		go func() {
-			defer wg.Done()
+		ins := []*wire.OutPoint{connectorInput, vtxoInput}
+		outs := []*wire.TxOut{{
+			Value:    int64(vtxoAmount) + int64(connectorAmount) - int64(feeAmount),
+			PkScript: serverScript,
+		}}
 
-			for job := range jobs {
-				go func(i int, connectorInput *wire.OutPoint, connectorPrevout *wire.TxOut) {
-					defer wg.Done()
-
-					ins := []*wire.OutPoint{connectorInput, vtxoInput}
-					outs := []*wire.TxOut{{
-						Value:    int64(vtxoAmount) + int64(connectorAmount) - int64(feeAmount),
-						PkScript: serverScript,
-					}}
-
-					vtxoSequence := wire.MaxTxInSequenceNum
-					if txLocktime != 0 {
-						vtxoSequence = wire.MaxTxInSequenceNum - 1
-					}
-					txSequence := []uint32{wire.MaxTxInSequenceNum, vtxoSequence}
-
-					partialTx, err := psbt.New(
-						ins, outs, version, txLocktime, txSequence,
-					)
-					if err != nil {
-						chErr <- err
-						return
-					}
-
-					updater, err := psbt.NewUpdater(partialTx)
-					if err != nil {
-						chErr <- err
-						return
-					}
-
-					if err := updater.AddInWitnessUtxo(connectorPrevout, 0); err != nil {
-						chErr <- err
-						return
-					}
-
-					if err := updater.AddInWitnessUtxo(&wire.TxOut{
-						Value:    int64(vtxoAmount),
-						PkScript: vtxoScript,
-					}, 1); err != nil {
-						chErr <- err
-						return
-					}
-
-					if err := updater.AddInSighashType(txscript.SigHashDefault, 1); err != nil {
-						chErr <- err
-						return
-					}
-
-					chTxs <- chTx{i, partialTx}
-				}(job.i, job.connectorInput, job.connectorPrevout)
-			}
-		}()
-	}
-
-	for i := range connectors {
-		select {
-		case err := <-chErr:
-			return nil, err
-		default:
-			connectorPrevout := prevouts[i]
-			connectorInput := connectors[i]
-			jobs <- chJob{i, connectorInput, connectorPrevout}
+		vtxoSequence := wire.MaxTxInSequenceNum
+		if txLocktime != 0 {
+			vtxoSequence = wire.MaxTxInSequenceNum - 1
 		}
-	}
-	close(jobs)
-	wg.Wait()
 
-	count := 0
-	for {
-		select {
-		case err := <-chErr:
-			close(chErr)
+		partialTx, err := psbt.New(
+			ins,
+			outs,
+			version,
+			txLocktime,
+			[]uint32{wire.MaxTxInSequenceNum, vtxoSequence},
+		)
+		if err != nil {
 			return nil, err
-		case res := <-chTxs:
-			forfeitTxs[res.i] = res.tx
-			count++
-			if count == len(connectors) {
-				close(chTxs)
-				close(chErr)
-				return forfeitTxs, nil
-			}
 		}
+
+		updater, err := psbt.NewUpdater(partialTx)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := updater.AddInWitnessUtxo(connectorPrevout, 0); err != nil {
+			return nil, err
+		}
+
+		if err := updater.AddInWitnessUtxo(&wire.TxOut{
+			Value:    int64(vtxoAmount),
+			PkScript: vtxoScript,
+		}, 1); err != nil {
+			return nil, err
+		}
+
+		if err := updater.AddInSighashType(txscript.SigHashDefault, 1); err != nil {
+			return nil, err
+		}
+
+		forfeitTxs = append(forfeitTxs, partialTx)
 	}
+
+	return forfeitTxs, nil
 }
 
 func getConnectorInputs(partialTx *psbt.Packet, connectorAmount int64) ([]*wire.OutPoint, []*wire.TxOut) {
@@ -131,15 +81,4 @@ func getConnectorInputs(partialTx *psbt.Packet, connectorAmount int64) ([]*wire.
 	}
 
 	return inputs, witnessUtxos
-}
-
-type chJob struct {
-	i                int
-	connectorInput   *wire.OutPoint
-	connectorPrevout *wire.TxOut
-}
-
-type chTx struct {
-	i  int
-	tx *psbt.Packet
 }
