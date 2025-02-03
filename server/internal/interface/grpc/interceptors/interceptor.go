65290349b5eb8cebdb5611caa85533b89abdce3c
altafan <18440657+altafan@users.noreply.github.com>
2025-02-03 14:27:05 +0100

    Exclude recovery interceptor

--- server/internal/interface/grpc/interceptors/interceptor.go
+++ server/internal/interface/grpc/interceptors/interceptor.go
@@ -9,7 +9,7 @@ import (
 // UnaryInterceptor returns the unary interceptor
 func UnaryInterceptor(svc *macaroons.Service) grpc.ServerOption {
 	return grpc.UnaryInterceptor(middleware.ChainUnaryServer(
-		unaryPanicRecoveryInterceptor(),
+		// unaryPanicRecoveryInterceptor(),
 		unaryLogger,
 		unaryMacaroonAuthHandler(svc),
 	))
@@ -18,7 +18,7 @@ func UnaryInterceptor(svc *macaroons.Service) grpc.ServerOption {
 // StreamInterceptor returns the stream interceptor with a logrus log
 func StreamInterceptor(svc *macaroons.Service) grpc.ServerOption {
 	return grpc.StreamInterceptor(middleware.ChainStreamServer(
-		streamPanicRecoveryInterceptor(),
+		// streamPanicRecoveryInterceptor(),
 		streamLogger,
 		streamMacaroonAuthHandler(svc),
 	))
