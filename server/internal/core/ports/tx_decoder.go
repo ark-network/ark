34d84f9afb1d2a03a795f66f82351940cc1c06d2
altafan <18440657+altafan@users.noreply.github.com>
2025-05-19 15:38:26 +0200

    Add ports.TxDecoder

---
+++ server/internal/core/ports/tx_decoder.go
@@ -0,0 +1,14 @@
+package ports
+
+import "github.com/ark-network/ark/server/internal/core/domain"
+
+type TxIn domain.VtxoKey
+
+type TxOut struct {
+	Amount   uint64
+	PkScript []byte
+}
+
+type TxDecoder interface {
+	DecodeTx(tx string) (txid string, ins []TxIn, outs []TxOut, err error)
+}
---
+++ server/internal/infrastructure/tx-decoder/bitcoin/service.go
@@ -0,0 +1,39 @@
+package bitcointxdecoder
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/ark-network/ark/server/internal/core/ports"
+	"github.com/btcsuite/btcd/btcutil/psbt"
+)
+
+type service struct{}
+
+func NewService() ports.TxDecoder {
+	return &service{}
+}
+
+func (s *service) DecodeTx(tx string) (string, []ports.TxIn, []ports.TxOut, error) {
+	ptx, err := psbt.NewFromRawBytes(strings.NewReader(tx), true)
+	if err != nil {
+		return "", nil, nil, fmt.Errorf("failed to parse partial tx: %s", err)
+	}
+
+	txid := ptx.UnsignedTx.TxHash().String()
+	ins := make([]ports.TxIn, 0, len(ptx.UnsignedTx.TxIn))
+	for _, input := range ptx.UnsignedTx.TxIn {
+		ins = append(ins, ports.TxIn{
+			Txid: input.PreviousOutPoint.Hash.String(),
+			VOut: input.PreviousOutPoint.Index,
+		})
+	}
+	outs := make([]ports.TxOut, 0, len(ptx.UnsignedTx.TxOut))
+	for _, output := range ptx.UnsignedTx.TxOut {
+		outs = append(outs, ports.TxOut{
+			Amount:   uint64(output.Value),
+			PkScript: output.PkScript,
+		})
+	}
+	return txid, ins, outs, nil
+}
