7b82c08629dee052490abde232e943fa9287a401
sukrucildirr <sukrucildirr@gmail.com>
2025-06-04 17:58:45 +0300

    Update db.go
    Signed-off-by: sukrucildirr <sukrucildirr@gmail.com>

--- server/pkg/kvdb/etcd/db.go
+++ server/pkg/kvdb/etcd/db.go
@@ -79,7 +79,7 @@ func (c *commitStatsCollector) PrintStats() string {
 	return s
 }
 
-// updateStatMap updatess commit stats map for a caller.
+// updateStatMap updates commit stats map for a caller.
 func updateStatMap(
 	caller string, stats CommitStats, m map[string]*callerStats) {
 
