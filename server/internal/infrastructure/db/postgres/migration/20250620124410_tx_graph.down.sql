150b63cee7294563cc35163a8db7ec7e5f9de4d1
louisinger <louis@vulpem.com>
2025-06-20 15:23:30 +0200

    postgres migration: drop view before altering tables

--- server/internal/infrastructure/db/postgres/migration/20250620124410_tx_graph.down.sql
+++ server/internal/infrastructure/db/postgres/migration/20250620124410_tx_graph.down.sql
@@ -1,17 +1,18 @@
+DROP VIEW IF EXISTS round_commitment_tx_vw;
+DROP VIEW IF EXISTS round_tx_vw;
+
+ALTER TABLE tx DROP COLUMN children JSONB;
+
 ALTER TABLE tx ADD COLUMN tree_level INTEGER;
 ALTER TABLE tx ADD COLUMN parent_txid VARCHAR;
 ALTER TABLE tx ADD COLUMN is_leaf BOOLEAN;
 
-ALTER TABLE tx DROP COLUMN children JSONB;
-
-DROP VIEW IF EXISTS round_tx_vw;
 CREATE VIEW round_tx_vw AS
 SELECT tx.*
 FROM round
 LEFT OUTER JOIN tx
 ON round.id=tx.round_id;
 
-DROP VIEW IF EXISTS round_commitment_tx_vw;
 CREATE VIEW round_commitment_tx_vw AS
 SELECT round.*, tx.*
 FROM round
--- server/internal/infrastructure/db/postgres/migration/20250620124410_tx_graph.up.sql
+++ server/internal/infrastructure/db/postgres/migration/20250620124410_tx_graph.up.sql
@@ -1,18 +1,19 @@
+DROP VIEW IF EXISTS round_commitment_tx_vw;
+DROP VIEW IF EXISTS round_tx_vw;
+
 ALTER TABLE tx DROP COLUMN tree_level;
 ALTER TABLE tx DROP COLUMN parent_txid;
 ALTER TABLE tx DROP COLUMN is_leaf;
 
 ALTER TABLE tx ADD COLUMN children JSONB;
 
-DROP VIEW IF EXISTS round_commitment_tx_vw;
-CREATE OR REPLACE VIEW round_commitment_tx_vw AS
+CREATE VIEW round_commitment_tx_vw AS
 SELECT round.*, tx.*
 FROM round
 INNER JOIN tx
 ON round.id = tx.round_id AND tx.type = 'commitment';
 
-DROP VIEW IF EXISTS round_tx_vw;
-CREATE OR REPLACE VIEW round_tx_vw AS
+CREATE VIEW round_tx_vw AS
 SELECT tx.*
 FROM round
 LEFT OUTER JOIN tx
